2ad0dafb21c3ff95c31424102e98182102acfb1e
PAVITHRA5898 <54078566+PAVITHRA5898@users.noreply.github.com>
2022-03-03 17:00:33 +0530

    Add files via upload

---
+++ Pavithra_DataStructureAssignmentSolution/src/com/datastructure/construction/Main.java
@@ -0,0 +1,16 @@
+package com.datastructure.construction;
+
+public class Main {
+
+    public static int totalFloors;
+    public static int[] totalFloorSize;
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Driver myDriverObj = new Driver();
+        myDriverObj.getInputs();
+        myDriverObj.calculateAndPrintOrderOfConstruction(myDriverObj.totalFloorSize);
+    }
+}
